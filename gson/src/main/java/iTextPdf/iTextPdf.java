1e61c4ce2e05246211a6e8f2ec77f808c03dd45c
Pete <swanseatechsupport@hotmail.co.uk>
2017-12-06 19:34:06 +0000

    test

--- gson/src/main/java/iTextPdf/iTextPdf.java
+++ gson/src/main/java/iTextPdf/iTextPdf.java
@@ -1,12 +1,10 @@
 package iTextPdf;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.PageSize;
@@ -17,24 +15,24 @@ public class iTextPdf {
 
 	/**
 	 * @param args
-	 * @throws FileNotFoundException 
-	 * @throws DocumentException
+	 * @throws Exception
 	 */
-	public static void main(String[] args) throws FileNotFoundException, DocumentException {
+	public static void main(String[] args) throws Exception {
 
 		// Instantiation of document object
 		Document document = new Document(PageSize.A4, 50, 50, 50, 50);
 
 		// Creation of PdfWriter object
-		@SuppressWarnings("unused")
-		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("sampleFile.pdf"));
-		document.open();
+		try (FileOutputStream fos = new FileOutputStream("sampleFile.pdf")) {
+			PdfWriter.getInstance(document, fos);
+			document.open();
+		} 
 
 		// Creation of chapter object and main heading
-		Paragraph mainTitle = new Paragraph("Document Reprint - Scanned Jobs", 
-				 FontFactory.getFont(FontFactory.COURIER, 18, Font.BOLD));
+		Paragraph mainTitle = new Paragraph("Document Reprint - Scanned Jobs",
+				FontFactory.getFont(FontFactory.COURIER, 18, Font.BOLD));
 		document.add(mainTitle);
-		
+
 		// Creation of section object with timestamp and userID
 		Paragraph subTitle = new Paragraph("Scanned on 03/11/2017 @ 14:43:39 by celodwig",
 				FontFactory.getFont(FontFactory.COURIER, 16, Font.BOLD));
@@ -43,12 +41,11 @@ public class iTextPdf {
 		document.add(subTitle);
 
 		// Creation of reprint docs list
-		List<String> list = Arrays.asList("1398734010 01552 (Single)", 
-        		"1398734010 01553 (Single)",
-        		"1398734010       (Batch)");
-        for (String item : list) {
-        	document.add(new Paragraph(item));
-        }
+		List<String> list = Arrays.asList("1398734010 01552 (Single)", "1398734010 01553 (Single)",
+				"1398734010       (Batch)");
+		for (String item : list) {
+			document.add(new Paragraph(item));
+		}
 
 		// Close the main document
 		document.close();
